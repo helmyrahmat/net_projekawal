13c74e72a1b0a0641ec4c2a27846fc17970fe83a
helmyrahmat <helmyrahmat5@gmail.com>
2025-03-19 11:50:24 +0700

    tambahbaris

--- src/hello01/Hello01.java
+++ src/hello01/Hello01.java
@@ -16,7 +16,7 @@ public class Hello01 {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello Teman");
+        System.out.println("Hello teman");
     }
     
 }
